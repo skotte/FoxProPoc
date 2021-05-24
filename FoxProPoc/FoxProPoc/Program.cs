cdb4e8cbc6ab8abd1b590293759e8fee2aa83964
Sreenivas <kotte.sri@gmail.com>
2021-05-24 15:38:46 -0400

    Updates for update fox pro data

--- FoxProPoc/FoxProPoc/Program.cs
+++ FoxProPoc/FoxProPoc/Program.cs
@@ -16,12 +16,13 @@ namespace ConsoleAppConnectDB
     {
         static void Main(string[] args)
         {
-            var dbFileName = "Company.dbc";
+            var dbFileName = "bmsw.dbc";
             Console.WriteLine("Enter FoxPro database folder location");
             var folderPath = Console.ReadLine();
             if(string.IsNullOrWhiteSpace(folderPath) || !Directory.Exists(folderPath))
             {
-                folderPath = "C:\\Users\\Sreenivas\\Documents\\Visual Foxpro 6.0\\Visual Foxpro 6.0";
+                //folderPath = "C:\\Users\\Sreenivas\\Documents\\Visual Foxpro 6.0\\Visual Foxpro 6.0";
+                folderPath = "C:\\Users\\Sreenivas\\Documents\\Stretto\\TW_1133_Blank_Data\\data\\";
             }
             var fullPath = Path.Combine(folderPath, dbFileName);
              
@@ -68,6 +69,7 @@ namespace ConsoleAppConnectDB
             var doc = new System.Xml.XmlDocument();
 
             doc.Load(filepath);
+            
             var adapter = new OleDbDataAdapter();
             var tableNodes = doc.SelectNodes("ScrambleTable/Table");
             var connection = new OleDbConnection($"Provider=VFPOLEDB.1;Data Source={dbFilePath};mode=share exclusive");
@@ -76,12 +78,13 @@ namespace ConsoleAppConnectDB
             {
                 var result = new DataTable();
                 var tblName = tbl.Attributes["Name"].Value;
-                var primaryKey = tbl.Attributes["PrimaryKey"].Value;
+                var primaryKey = tbl.Attributes["PrimaryKey"].Value.Split(',');
+                //var keys = primaryKey.Split(',');
                 var selectSql = new OleDbCommand($"select * from {tblName}", connection);
                 adapter.SelectCommand = selectSql;
                 adapter.Fill(result);
-                var primaryKeyValue = string.Empty;
-                
+                //var primeKeyCol = new Dictionary<string, object>();
+                var primeKeyDetails = new Dictionary<string, Tuple<string,object>>();
                 foreach (DataRow dataRow in result.Rows)
                 {                     
                     var subSql = new StringBuilder();
@@ -89,22 +92,49 @@ namespace ConsoleAppConnectDB
                     {
                         var column = child.Attributes["Name"].Value;
                         var algorithm = child.Attributes["ScramblingAlgorithm"].Value;
-                        primaryKeyValue = dataRow[primaryKey].ToString();
+                        var columnDataType = result.Columns[column].DataType.Name;
+
+                        //primaryKeyValue = dataRow[primaryKey].ToString();
                         var newValue = ScrambleColumnValue(dataRow[column].ToString(), algorithm);
 
                         if (!string.IsNullOrEmpty(subSql.ToString())) subSql.Append(",");
-                        subSql.Append($" {tblName}.{column} =  \"{newValue}\" "); 
+                        var newVal = columnDataType == "String" ? $"\"{newValue}\"" : newValue;
+
+                        subSql.Append($" {tblName}.{column} = {newVal} "); 
+                    }
+
+                    foreach (var key in primaryKey)
+                    {
+                        var columnDataType = result.Columns[key].DataType.Name;
+                        //primeKeyCol.Add(key, dataRow[key]);
+                        primeKeyDetails.Add(key, new Tuple<string, object>(columnDataType, dataRow[key]));
                     }
 
                     connection.Open();
-                    var sql = $"UPDATE {tblName} SET {subSql.ToString()} WHERE { primaryKey} = {primaryKeyValue}";
+                    var sql = $"UPDATE {tblName} SET {subSql.ToString()} WHERE { BuildWhereClause(primeKeyDetails)}";
+                    //var sql = $"UPDATE {tblName} SET {subSql.ToString()} WHERE { primaryKey} = {primaryKeyValue}";
                     var updateSql = new OleDbCommand(sql, connection);
                     adapter.UpdateCommand = updateSql;
                     adapter.UpdateCommand.ExecuteNonQuery();
                     connection.Close();
                 }
             }           
-        }        
+        }
+
+        
+
+        private static string BuildWhereClause(Dictionary<string, Tuple<string, object>> primeKeyCol)
+        {
+            var sql = new StringBuilder();
+            foreach (var primary in primeKeyCol)
+            {
+                if (!string.IsNullOrEmpty(sql.ToString())) sql.Append(" AND ");
+                var newVal = primary.Value.Item1 == "String" ? $"\"{primary.Value.Item2}\"" : primary.Value.Item2;
+                sql.Append($"{ primary.Key} = {newVal}");
+            }
+
+            return sql.ToString();
+        }
 
         private static string ScrambleColumnValue(string columnValue, string algorithm)
         {
@@ -155,6 +185,19 @@ namespace ConsoleAppConnectDB
             ch[index] = Convert.ToChar(newValue.ToString());
              
             return new string(ch);
+        }
+
+        private static void InsertData(string dbFilePath)
+        {
+            var adapter = new OleDbDataAdapter();
+            var connection = new OleDbConnection($"Provider=VFPOLEDB.1;Data Source={dbFilePath};mode=share exclusive");
+            connection.Open();
+
+            var insertSql = new OleDbCommand("Insert into Accounts (case_no, account_no, bank_code, type, status, bankstatus, descr, ledger_bal, bankbk_bal, bankav_bal,banksh_bal, accr_int, opened, closed, last_recon, last_activ, total_dep, total_disb, next_check, next_dep,next_memo, next_adj, bnkbkbal01, bnkbkbal02, bnkbkbal03, bnkbkbal04, bnkbkbal05, bnkbkbal06, bnkbkbal07, bnkbkbal08,bnkbkbal09, bnkbkbal10, bnkbkbal11, bnkbkbal12, int_rate, term, principal, is_special, inst_code, mat_date,est_value, rollover, tickle, ent_by, ent_date, ent_time, approved, appr_by, sent_open, sent_otime,sent_close, sent_ctime, order, int_type, tda_appr, recon_done, reconcile_fl, rolldisp, rollconf, rollprinc,rollterm, rollby, rollsent, rollnewterm, rollnewprinc, backupwithholding, rollnewtransferaccount, lastbankmaturity, rate_code, dep_ledg_cat_key) values  (\"123456789012345\", \"7896542315\", \"1234\", \"DDA\", \"O\", \"O\", \"Chase Bank\",11,12,13,14,1234,{d'1968-04-28'},{d'1968-04-28'},{d'1968-04-28'},{d'1968-04-28'},1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,15,16,17,1,\"1234\",{ d'1968-04-28'},1111,{ d'1968-04-28'},123,\"12345678\",{d'1968-04-28'},\"12345\",\"A\",\"12345678\",{d'1968-04-28'},\"12345\", {d'1968-04-28'},\"12345\",\"123\",\"C\",\"1234567890\",{d'1968-04-28'},1,{d'1968-04-28'},\"1\",123456,123,\"12345678\",{d'1968-04-28'},123,123,1,\"123456\",{d'1968-04-28'},\"12345\",\"12345\")", connection);
+
+            adapter.InsertCommand = insertSql;
+            adapter.InsertCommand.ExecuteNonQuery();
+            connection.Close();
         }
     }
 }
